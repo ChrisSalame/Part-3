887875296d064c20cdfcc51e8a55b9100ec5e1d9
Christian Salame <salamec@shernet.sheridancollege.ca>
2024-03-13 11:38:22 -0400

    Added Demo Script, This was brought in through questions asked in class.

---
+++ Assets/Week 9/Scripts/Demo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+
+    //KeyValuePair<string, int> myPair;
+    //Dictionary<string, int> myDictionary;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+      //  myDictionary = new Dictionary<string, int>();
+      //  myDictionary.Add("Orcs", 17);
+      //  myDictionary.Add("Goblin", 17);
+      //  myDictionary["Goblin"]+= 1;
+      //  for (int i = 0; i < myDictionary["Goblins"]; i++)
+      //  { 
+
+      //  }
+    //}
+
+ 
+}
