111e8158d5ef94e6bc628656b72b8be9c8ccb2de
Christian Salame <salamec@shernet.sheridancollege.ca>
2024-03-13 12:29:29 -0400

    Compleated task 4a, thief now dashes and shoots out 2 daggers when attacking

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -7,6 +7,8 @@ public class Thief : Villager
     public GameObject knifePrefab;
     public Transform spawnPointOne;
     public Transform spawnPointTwo;
+    public Transform dashLocal;
+
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
@@ -14,10 +16,14 @@ public class Thief : Villager
 
     protected override void Attack()
     {
+
         destination = transform.position;
         base.Attack();
         Instantiate(knifePrefab, spawnPointOne.position, spawnPointOne.rotation);
         Instantiate(knifePrefab, spawnPointTwo.position, spawnPointTwo.rotation);
+
+        transform.position = dashLocal.position;
+
     }
 
 }
