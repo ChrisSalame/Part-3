707b224044fa84205c70e73e69773fcb90fbe2a7
ROTOM\chris <salamec@sheridancollege.ca>
2024-04-01 01:07:30 -0400

    Got a basic version of 3 different creature placeholders spawning when space is clicked. Placeholders spawn randomly and got the base movement code that will be inherited later as a base

--- Assets/Assignment/Script/AquaticAnimal.cs
+++ Assets/Assignment/Script/AquaticAnimal.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AquaticAnimal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //This sets the speed
+    public float swimSpeed = 0.1f;
+    Vector2 swimDirection;
     void Start()
     {
-        
+        //This makes it so that the animal doesn't go updward, then the .x sets the swim direction to x
+        swimDirection.y = 0;
+        swimDirection.x = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        //This has the creature move in a set direction using speed
+        transform.Translate(swimDirection.normalized * swimSpeed);
     }
 }
---
+++ Assets/Assignment/Script/AquaticSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AquaticSpawner : MonoBehaviour
+{
+    public GameObject[] creaturePrefab = new GameObject[3];
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) 
+        {
+            Debug.Log("space works and spawner spawns");
+            Instantiate(creaturePrefab[Random.Range(0,3)]);
+        }
+        
+    }
+}
