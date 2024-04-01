7ab3d258215600d4c944601e5e99ba9f01521798
Christian Salame <salamec@shernet.sheridancollege.ca>
2024-04-01 12:43:38 -0400

    Added in first animal code, working on first animals inheritance

--- Assets/Assignment/Script/AquaticAnimal.cs
+++ Assets/Assignment/Script/AquaticAnimal.cs
@@ -8,11 +8,15 @@ public class AquaticAnimal : MonoBehaviour
     //This sets the speed
     public float swimSpeed = 0.1f;
     Vector2 swimDirection;
+
+    Rigidbody2D animalRb;
     void Start()
     {
-        //This makes it so that the animal doesn't go updward, then the .x sets the swim direction to x
+        //This makes it so that the animal doesn't go updward by setting y to 0, then the .x sets the swim direction to x
         swimDirection.y = 0;
         swimDirection.x = 1;
+
+        animalRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -26,6 +30,15 @@ public class AquaticAnimal : MonoBehaviour
         //This has the creature move in a set direction using speed
         transform.Translate(swimDirection.normalized * swimSpeed);
     }
+
+
+    //This function is to have the animal perform an action when pressed on 
+    public static void action() 
+    {
+
+
+    }
+
 
     //This destroys the game object once its offscreen
     private void OnBecameInvisible()
