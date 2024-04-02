6c7c3c4fbda20001e01845cf87d6b8ae1c804ed1
ROTOM\chris <salamec@sheridancollege.ca>
2024-04-01 23:38:46 -0400

    Added comments to code

--- Assets/Assignment/Script/AquaticSpawner.cs
+++ Assets/Assignment/Script/AquaticSpawner.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class AquaticSpawner : MonoBehaviour
 {
+    //This is creating a list
     public GameObject[] creaturePrefab = new GameObject[3];
     
     
     void Update()
     {
+        // This makes it so that when the down key is pressed, the player spawns one of the random prefabs
         if (Input.GetKeyDown("space")) 
         {
             Debug.Log("space works and spawner spawns");
