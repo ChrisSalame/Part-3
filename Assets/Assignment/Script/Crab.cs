6c3e8bff68bf1cc3f65cc9135252d9ba6567a63e
ROTOM\chris <salamec@sheridancollege.ca>
2024-04-02 20:23:34 -0400

    Crab now hides in shell when pressed on using the action function

--- Assets/Assignment/Script/Crab.cs
+++ Assets/Assignment/Script/Crab.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class Crab : AquaticAnimal
 {
     Coroutine pauseBeforeShell;
-    public Sprite[] crabSprite;
+    public Sprite[] crabSprite = new Sprite [2];
     protected override void action()
     {
-        GetComponent<SpriteRenderer>().sprite = crabSprite[0];
         pauseBeforeShell = StartCoroutine(actionCoroutine());
 
     }
 
     IEnumerator actionCoroutine()
     {
+        yield return new WaitForSeconds(2);
         GetComponent<SpriteRenderer>().sprite = crabSprite[1];
-        yield return new WaitForSeconds(1);
         Debug.Log("Crab Tucked into Shell");
     }
 
 
     private void OnMouseDown()
     {
+        GetComponent<SpriteRenderer>().sprite = crabSprite[1];
         action();
+
+
     }
 
 
